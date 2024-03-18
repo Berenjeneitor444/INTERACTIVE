eb1e50cc52ddf71c7c81dd21820122d6b001865a
Berenjeneitor444 <dciriano2@gmail.com>
2024-03-18 17:26:25 +0100

    formulario de contacto hecho

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -14,3 +14,27 @@ function menu(id) {
         }
     }
 }
+function validacion_email() {
+    let email = document.getElementById("email").value
+    let asunto = document.getElementById("asunto").value
+    let cuerpo = document.getElementById("cuerpo").value
+    if (!(/^\w+@\w+\.[a-zA-Z]{2,4}$/.test(email))) {
+        alert("formato de Email incorrecto")
+        return false
+    }
+    else if (asunto.length < 5 || !(/\w{3,}/.test(asunto))) {
+        //que tenga mas de 5 caracteres y una palabra de mas de tres caracteres alfanumericos
+        alert("el asunto es muy corto, incoherente o inexistente")
+        return false
+    }
+    else if (cuerpo.length < 15 || !(/\w{3,}/.test(cuerpo))) {
+        alert("cuerpo muy corto, incoherente o inexistente")
+        return false
+    }
+    else {
+        return true
+    }
+}
+function enviar_correo(){
+
+}
